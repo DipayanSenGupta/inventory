79f3b81f2aa2d7c8faf8aeae585a1bec7f66f6d0
DipayanSenGupta <dip.himu@gmail.com>
2019-09-17 17:26:54 +0600

    Restrict access to Logincontroller

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
+use Illuminate\Http\Request;
+use Auth;
 class LoginController extends Controller
 {
     /*
@@ -35,5 +37,39 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+        $this->middleware('guest:admin')->except('logout');
+        $this->middleware('guest:sale')->except('logout');
+
+    }
+    public function showAdminLoginForm(){
+        return view('auth.login',['url' => 'admin']);
+    }
+
+    public function adminLogin(Request $request){
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if(Auth::guard('admin')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+            return redirect()->intended('/admin');            
+        }
+        return back()->withInput($request->only('email','remember'))
+    }
+
+      public function showSaleLoginForm(){
+        return view('auth.login',['url' => 'sale']);
+    }
+
+    public function SaleLogin(Request $request){
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:6'
+        ]);
+
+        if(Auth::guard('sale')->attempt(['email' => $request->email, 'password' => $request->password], $request->get('remember'))) {
+            return redirect()->intended('/sale');            
+        }
+        return back()->withInput($request->only('email','remember'));
     }
 }
