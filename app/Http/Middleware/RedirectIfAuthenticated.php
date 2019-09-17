e80f3b8e28dac536563369daff68ea23988f6ed3
DipayanSenGupta <dip.himu@gmail.com>
2019-09-17 18:23:10 +0600

    Change default redirect to home page

--- app/Http/Middleware/RedirectIfAuthenticated.php
+++ app/Http/Middleware/RedirectIfAuthenticated.php
@@ -1,26 +1,33 @@
-<?php
+    <?php
 
-namespace App\Http\Middleware;
+    namespace App\Http\Middleware;
 
-use Closure;
-use Illuminate\Support\Facades\Auth;
+    use Closure;
+    use Illuminate\Support\Facades\Auth;
 
-class RedirectIfAuthenticated
-{
+    class RedirectIfAuthenticated
+    {
     /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @param  string|null  $guard
-     * @return mixed
-     */
+    * Handle an incoming request.
+    *
+    * @param  \Illuminate\Http\Request  $request
+    * @param  \Closure  $next
+    * @param  string|null  $guard
+    * @return mixed
+    */
     public function handle($request, Closure $next, $guard = null)
     {
-        if (Auth::guard($guard)->check()) {
-            return redirect('/home');
-        }
 
-        return $next($request);
+    if ($guard == "admin" && Auth::guard($guard)->check()) {
+    return redirect('/admin');
     }
-}
+    if ($guard == "sale" && Auth::guard($guard)->check()) {
+    return redirect('/sale');
+    }
+    if (Auth::guard($guard)->check()) {
+    return redirect('/home');
+    }
+
+    return $next($request);
+    }
+    }
